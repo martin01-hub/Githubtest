ba6d507bf07753905a5725134649c637f50bdff3
Martin Arinze <arinzemartin68@gmail.com>
2022-10-07 12:52:23 +0100

    initial commit

---
+++ Tutorial.py
@@ -0,0 +1,45 @@
+from cmath import sqrt
+import math
+from unicodedata import name
+
+# name = 'tim'
+
+# print('tim you are welcome')
+# print('tim are you having headache?')
+# print('tim you are mad')
+
+# print (name)
+
+# name = 'tim'
+
+# print('Hi. \nHow are you?')
+# print(name.lower().islower())
+
+
+# print(len(name))
+# print(name.index('i'))
+# print(name.replace('t', 'j'))
+
+# GETTING USER'S INPUT
+
+# name = input('input your Name: ')
+# age = input('input your age: ')
+# print(name, 'you are', age, 'years old')
+
+# NUMBERS IN PYTHON
+
+# print(min(5, 4, 6, 3, 10))
+# print(max(10, 5, 79, 20))
+# print(round(4.8))
+# print(bin(334))
+# from math import *
+
+# print(sqrt(4))
+
+Sentence = input('What is your sentenece: ')
+# print(Sentence)
+print('Your sentence is', Sentence)
+pull = input('what is the word you want to replace: ')
+push = input('select replacement: ')
+print(Sentence.replace(pull, push))
+
---
+++ Tutorial2.py
@@ -0,0 +1,46 @@
+# Building a simple word replacement program
+
+# sentence = input('Enter your sentence: ')
+# print(sentence)
+# print('Your sentence is', sentence)
+# banner1 = input('Enter the word you want to replace: ')
+# banner2 = input('Enter the replacement word: ')
+# print(sentence.replace(banner1, banner2))
+
+# Working with lists
+
+# Countries = ['AMERICA', 'RUSSIA', 'FRANCE', 'GERMANY']
+# print(type(Countries))
+# countries = list(('nigeria', 34, False)) 
+# print(countries)
+
+#  LIST ATTRIBUTES
+
+# list1 = [1, 2, 3, 4, 5]
+# list2 = ['mango', 'orange', 'banana', 'melon']
+# list1.extend(list2)
+# list2.append('cherry')
+# list2.insert(1, 'cherry')
+# list2.remove('banana')
+
+# print(list2.index('melon'))
+# print(list2.count('melon'))
+# list1.sort()
+# list3 = list2.copy()
+# print(list3)
+
+# pop is used to remove words from lists
+
+# TUPLES IN PYTHON
+
+# three_numbers = tuple((1, 'home', True, 2, 3))
+# boo = (True, False, True)
+# print(three_numbers) 
+
+# FUNCTIONS IN PYTHON
+
+def greetings_functions():
+    print('welcome user')
+
+greetings_functions()
+
